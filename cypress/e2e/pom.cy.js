e71dc1fe4da241da022b5025855e22d6a318c8d9
Olalla Salgado <olasalgado@gmail.com>
2025-07-23 01:47:44 +0200

    agregandopromytest

--- cypress/e2e/pom.cy.js
+++ cypress/e2e/pom.cy.js
@@ -11,13 +11,38 @@ describe('ToDoMVC - Tests básicos con POM', () => {
 
 
   it('añadir tarea nueva ', () => {
+
     todo.añadirtareanueva('pasear al perro');
     
   });
 
   it('completar tarea', () => {
+
     todo.añadirtareanueva('pasear al perro'); 
     todo.completartarea('pasear al perro ');  
   });
-    
-  });
+
+
+  it('desmarcar tarea completada', () => {
+
+    todo.añadirtareanueva('pasear al perro'); 
+    todo.completartarea('pasear al perro');  
+    todo.desmarcartareacompletada('pasear al perro');  
+
+
+})
+ 
+
+it('editar tarea', () => {
+
+  todo.editartarea(0, 'nuevo texto')
+
+  })
+
+  
+it('borrar tareas completadas', () => {
+
+  todo.borrartareascompletadas(0, 'pasear al perro')
+
+})
+})
